577b059ea2d8afe712b835ad5cd8754a7ed177f2
juliancesar <juliancesar@gmail.com>
2013-05-07 10:54:11 -0300

    Estruturação dos projetos Utilização de SPI

--- impl/core/src/main/java/br/gov/frameworkdemoiselle/behave/controller/EngineController.java
+++ impl/core/src/main/java/br/gov/frameworkdemoiselle/behave/controller/EngineController.java
@@ -36,6 +36,31 @@
  */
 package br.gov.frameworkdemoiselle.behave.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import br.gov.frameworkdemoiselle.behave.parser.Parser;
+import br.gov.frameworkdemoiselle.behave.runner.Runner;
+import br.gov.frameworkdemoiselle.behave.util.Dependencies;
+
 public class EngineController {
+
+	private Parser parser;
+	private Runner runner;
+	private Logger logger = Logger.getLogger(this.toString());
+
+	public void run() throws Throwable {
+
+		logger.log(Level.INFO, "Iniciou o processo...");
+
+		parser = (Parser) Dependencies.getInstance().getInstanceDependecy(Parser.class);
+		parser.run();
+
+		runner = (Runner) Dependencies.getInstance().getInstanceDependecy(Runner.class);
+		runner.run();
+
+		logger.log(Level.INFO, "Concluiu o processo.");
+
+	}
 
 }
