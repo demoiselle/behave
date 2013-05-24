1e021882a95d4b48bd650da15c7e4a991c08d1c0
juliancesar <juliancesar@gmail.com>
2013-05-24 16:26:09 -0300

    Pequena correção

--- sample/serpro-treino/src/test/java/demoisellebehave/serpro/treino/test/steps/MySteps.java
+++ sample/serpro-treino/src/test/java/demoisellebehave/serpro/treino/test/steps/MySteps.java
@@ -36,8 +36,6 @@
  */
 package demoisellebehave.serpro.treino.test.steps;
 
-import org.jbehave.core.annotations.Given;
-
 import br.gov.frameworkdemoiselle.behave.parser.Step;
 
 public class MySteps implements Step {
