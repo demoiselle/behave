ac39fc629e063ec0eb763e17971c008b191bb603
Vanderson <botelhojp@gmail.com>
2013-05-27 12:10:57 -0300

    criacao do projeto plugin eclipse

---
+++ plugin/plugin-eclipse/src/main/java/br/gov/frameworkdemoiselle/behave/plugin/eclipse/App.java
@@ -0,0 +1,5 @@
+package br.gov.frameworkdemoiselle.behave.plugin.eclipse;
+
+public class App{
+	
+}
---
+++ plugin/plugin-eclipse/src/test/java/br/gov/frameworkdemoiselle/behave/plugin/eclipse/AppTest.java
@@ -0,0 +1,11 @@
+package br.gov.frameworkdemoiselle.behave.plugin.eclipse;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+	}
+
+}
