b6059b7f5f6fd19471a92beb96cf757e052766a5
juliancesar <juliancesar@gmail.com>
2013-05-28 12:26:23 -0300

    Retirada de warning

--- sample/serpro-treino/src/test/java/demoisellebehave/serpro/treino/Obras.java
+++ sample/serpro-treino/src/test/java/demoisellebehave/serpro/treino/Obras.java
@@ -48,7 +48,7 @@ public class Obras {
 	public Obras() {
 		eng.addSteps(new MySteps());
 	}
-	
+
 	@Test
 	public void testObras() throws Throwable {
 		eng.addStories("/stories/acesso/");
@@ -57,4 +57,3 @@ public class Obras {
 	}
 
 }
-
