919223320da1c91829a7ccb15d5ff254c9f0e43f
Vanderson <botelhojp@gmail.com>
2013-06-04 18:20:22 -0300

    remocao de for

--- impl/extension/integration/alm/src/test/java/br/gov/frameworkdemoiselle/behave/integration/alm/autenticator/AutenticatorClientTests.java
+++ impl/extension/integration/alm/src/test/java/br/gov/frameworkdemoiselle/behave/integration/alm/autenticator/AutenticatorClientTests.java
@@ -3,15 +3,13 @@ package br.gov.frameworkdemoiselle.behave.integration.alm.autenticator;
 public class AutenticatorClientTests {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++) {
-			AutenticatorClient autenticator = new AutenticatorClient(9990, "stct3.cta.serpro");
-			autenticator.open();
-			String user = autenticator.getUser();
-			String password = autenticator.getPassword();
-			System.out.println(user);
-			System.out.println(password);
-			autenticator.close();
-		}
+		AutenticatorClient autenticator = new AutenticatorClient(9990, "stct3.cta.serpro");
+		autenticator.open();
+		String user = autenticator.getUser();
+		String password = autenticator.getPassword();
+		System.out.println(user);
+		System.out.println(password);
+		autenticator.close();
 	}
 
 }
