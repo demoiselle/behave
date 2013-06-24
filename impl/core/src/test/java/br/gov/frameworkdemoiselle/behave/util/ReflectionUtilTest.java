7a059d01d928c5b9ad2e7188006d9a8226988194
juliancesar <juliancesar@gmail.com>
2013-06-24 14:25:50 -0300

    Adicionada primeira verão do Data Container

--- impl/core/src/test/java/br/gov/frameworkdemoiselle/behave/util/ReflectionUtilTest.java
+++ impl/core/src/test/java/br/gov/frameworkdemoiselle/behave/util/ReflectionUtilTest.java
@@ -1,6 +1,5 @@
 package br.gov.frameworkdemoiselle.behave.util;
 
-import static org.junit.Assert.fail;
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -12,29 +11,29 @@ import br.gov.frameworkdemoiselle.behave.runner.ui.TextField;
 
 @ScreenMap(name = "Tela de Busca", location = "http://www.google.com.br")
 public class ReflectionUtilTest {
-	
+
 	@ElementMap(name = "Campo de Busca", locatorType = ElementLocatorType.Id, locator = "gbqfq")
 	private TextField searchField;
 
 	@Test
 	public void testGetLocation() {
-		String location=ReflectionUtil.getLocation("Tela de Busca");
+		String location = ReflectionUtil.getLocation("Tela de Busca");
 		Assert.assertEquals("http://www.google.com.br", location);
-		
+
 	}
 
 	@Test
 	public void testGetElementType() {
 		@SuppressWarnings("rawtypes")
-		Class type=ReflectionUtil.getElementType("Tela de Busca", "Campo de Busca");
+		Class type = ReflectionUtil.getElementType("Tela de Busca", "Campo de Busca");
 		Assert.assertTrue(type.getName().contains("TextField"));
 	}
 
 	@Test
 	public void testGetElementMap() {
-		ElementMap element=ReflectionUtil.getElementMap("Tela de Busca", "Campo de Busca");
+		ElementMap element = ReflectionUtil.getElementMap("Tela de Busca", "Campo de Busca");
 		Assert.assertTrue(element.toString().contains("name=Campo de Busca"));
-		
+
 	}
 
 }
