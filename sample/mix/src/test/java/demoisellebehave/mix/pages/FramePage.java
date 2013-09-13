3221fc323512db9668954452c1c74c5994725730
juliancesar <juliancesar@gmail.com>
2013-09-13 14:52:59 -0300

    Alterado o sample que chama um site que tem 20 frames/iframes.

--- sample/mix/src/test/java/demoisellebehave/mix/pages/FramePage.java
+++ sample/mix/src/test/java/demoisellebehave/mix/pages/FramePage.java
@@ -7,10 +7,8 @@ import br.gov.frameworkdemoiselle.behave.runner.ui.Link;
 
 @ScreenMap(name = "Principal", location = "http://pic.dhe.ibm.com/infocenter/clmhelp/v4r0m4/index.jsp")
 public class FramePage {
-	
-//	@Frame(name="frame01")
-	@ElementMap(name = "Introdução ao Rational Solution for CLM", locatorType = ElementLocatorType.XPath, locator = "id('tree_root')/x:div[1]/x:div/x:div[1]/x:div/x:div[3]/x:span/x:a")
+
+	@ElementMap(name = "Get started", locatorType = ElementLocatorType.XPath, locator = "//a[@href='../com.ibm.help.common.jazz.calm.doc/topics/c_get_started.html']")
 	private Link link1;
-	@ElementMap(name = "Introdução ao Gerenciamento de Mudança", locatorType = ElementLocatorType.XPath, locator = "id('c_get_started')/x:div[3]/x:div/x:div[1]/x:a")
-	private Link link2;
+
 }
--- sample/mix/src/test/resources/stories/frames/frames.story
+++ sample/mix/src/test/resources/stories/frames/frames.story
@@ -8,10 +8,5 @@ Desejo acessar o Google
 Cenário: Acesso a Tela com Frames
 
 Dado que vou para a tela "Principal"
-Então será exibido "Copyright IBM Corporation 2008"
-
-Quando clico em "Introdução ao Rational Solution for CLM"
-!--Então será exibido "Introdução ao Rational solution for Collaborative Lifecycle Management"
-
-!--Quando clico em "Introdução ao Gerenciamento de Mudança"
-!--Então será exibido "Introdução ao trabalho com o processo de gerenciamento de mudanças do projeto, itens de trabalho e integrações com outras ferramentas de gerenciamento de mudanças."
+Quando clico em "Get started"
+Então será exibido "Getting started"
