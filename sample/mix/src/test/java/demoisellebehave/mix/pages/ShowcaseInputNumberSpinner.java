1659ebb135570d8a154f76896175000324c9091f
juliancesar <juliancesar@gmail.com>
2014-03-10 14:07:21 -0300

    Correção do ElementMap do exemplo do Spinner

--- sample/mix/src/test/java/demoisellebehave/mix/pages/ShowcaseInputNumberSpinner.java
+++ sample/mix/src/test/java/demoisellebehave/mix/pages/ShowcaseInputNumberSpinner.java
@@ -5,11 +5,10 @@ import br.gov.frameworkdemoiselle.behave.annotation.ElementMap;
 import br.gov.frameworkdemoiselle.behave.annotation.ScreenMap;
 import br.gov.frameworkdemoiselle.behave.runner.webdriver.ui.richfaces4.RichInputNumberSpinner;
 
-@ScreenMap(name = "Showcase inputNumberSpinner", 
-	location = "http://showcase.richfaces.org/richfaces/component-sample.jsf?demo=inputNumberSpinner&skin=blueSky")
+@ScreenMap(name = "Showcase inputNumberSpinner", location = "http://showcase.richfaces.org/richfaces/component-sample.jsf?demo=inputNumberSpinner&skin=blueSky")
 public class ShowcaseInputNumberSpinner {
 
-	@ElementMap(name = "inputNumberSpinner", locatorType = ElementLocatorType.Id, locator = "j_idt1481")
+	@ElementMap(name = "inputNumberSpinner", locatorType = ElementLocatorType.XPath, locator = "//span[contains(concat(\" \",@class,\" \"),\" rf-insp \")][1]")
 	private RichInputNumberSpinner input;
 
 }
