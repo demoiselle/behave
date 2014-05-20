c70e28bcc2f4cfa9f0fc1c9b1ec1f6a4898451ed
juliancesar <juliancesar@gmail.com>
2014-05-20 14:25:33 -0300

    Correção de um bug com o JBehave que impedia que relatórios fossem gerados no Windows Adequação do código para a nova versão do JBehave

--- impl/parser/jbehave/src/main/java/br/gov/frameworkdemoiselle/behave/parser/jbehave/report/DefaultStoryReport.java
+++ impl/parser/jbehave/src/main/java/br/gov/frameworkdemoiselle/behave/parser/jbehave/report/DefaultStoryReport.java
@@ -41,6 +41,7 @@ import java.util.Map;
 
 import org.jbehave.core.model.ExamplesTable;
 import org.jbehave.core.model.GivenStories;
+import org.jbehave.core.model.Lifecycle;
 import org.jbehave.core.model.Meta;
 import org.jbehave.core.model.Narrative;
 import org.jbehave.core.model.OutcomesTable;
@@ -153,6 +154,11 @@ public class DefaultStoryReport implements StoryReporter {
 	}
 
 	public void pendingMethods(List<String> methods) {
+		
+	}
+
+	public void lifecyle(Lifecycle arg0) {
+		
 	}
 
 }
