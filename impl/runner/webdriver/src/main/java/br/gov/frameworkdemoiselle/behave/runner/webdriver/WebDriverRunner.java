da1d4d9105521133a09868ebaaa43d7927a53df8
Vanderson Botelho da Silva <vanderson.silva@serpro.gov.br>
2014-12-23 10:08:41 -0200

    log debug in navigateTo

--- impl/runner/webdriver/src/main/java/br/gov/frameworkdemoiselle/behave/runner/webdriver/WebDriverRunner.java
+++ impl/runner/webdriver/src/main/java/br/gov/frameworkdemoiselle/behave/runner/webdriver/WebDriverRunner.java
@@ -48,6 +48,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.eclipse.jetty.util.log.Log;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.UnsupportedCommandException;
@@ -102,6 +103,7 @@ public class WebDriverRunner implements Runner {
 
 	public void navigateTo(String url) {
 		try {
+			logger.debug(message.getString("message-try-access", url));
 			setDefaultWindow();
 			driver.navigate().to(url);
 		} catch (WebDriverException cause) {
