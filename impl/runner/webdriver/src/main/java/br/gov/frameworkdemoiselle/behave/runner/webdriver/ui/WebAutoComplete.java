9f22968250dd8d195dbd9a61a74c8823d5eee012
Julian Cesar dos Santos <julian.santos@serpro.gov.br>
2015-07-13 11:40:56 -0300

    Melhoria no funcionamento do WebSelect e WebAutoComplete. Agora eles aguardam o elemento ficar visível antes de cada click.

--- impl/runner/webdriver/src/main/java/br/gov/frameworkdemoiselle/behave/runner/webdriver/ui/WebAutoComplete.java
+++ impl/runner/webdriver/src/main/java/br/gov/frameworkdemoiselle/behave/runner/webdriver/ui/WebAutoComplete.java
@@ -52,16 +52,14 @@ import br.gov.frameworkdemoiselle.behave.runner.webdriver.WebDriverRunner;
  * @author SERPRO
  */
 public class WebAutoComplete extends WebBase implements AutoComplete {
-	
+
 	protected static BehaveMessage message = new BehaveMessage(WebDriverRunner.MESSAGEBUNDLE);
-	
+
 	/**
-	 * Armazena o valor que será selecionado na lista
-	 * do autocomplete, somente se, o valor de busca
-	 * for diferente deste
+	 * Armazena o valor que será selecionado na lista do autocomplete, somente
+	 * se, o valor de busca for diferente deste
 	 * 
-	 * Exemplo: Busca pelo texto "foo" mas seleciona
-	 * na lista o valor "foo, bar"
+	 * Exemplo: Busca pelo texto "foo" mas seleciona na lista o valor "foo, bar"
 	 * 
 	 * @see #select(String)
 	 */
@@ -73,20 +71,20 @@ public class WebAutoComplete extends WebBase implements AutoComplete {
 	@Override
 	public void selectByValue(String value) {
 		select(value);
-		
+
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void searchAndSelectValue(String searchValue, String selectValue) {
 		this.selectValue = selectValue;
-		
+
 		select(searchValue);
-		
+
 	}
-	
+
 	@Override
 	public String getText() {
 		waitElement(0);
@@ -96,36 +94,37 @@ public class WebAutoComplete extends WebBase implements AutoComplete {
 	/**
 	 * Método generalizado para selecionar o valor da lista
 	 * 
-	 * @param value Valor a ser informado no campo de texto do autocomplete
+	 * @param value
+	 *            Valor a ser informado no campo de texto do autocomplete
 	 */
 	protected void select(String value) {
-		
+
 		// Aguarda o primeiro elemento ser clicável
 		waitElement(0);
-		
+
 		List<WebElement> elements = getElements();
-		if(elements.size() == 1 && getElementMap().locator().length == 1){
-			throw new BehaveException(message.getString("exception-autocomplete-missing-elements","locator","@ElementMap"));
+		if (elements.size() == 1 && getElementMap().locator().length == 1) {
+			throw new BehaveException(message.getString("exception-autocomplete-missing-elements", "locator", "@ElementMap"));
 		}
-		
+
 		WebElement element = elements.get(0);
-		
-		if(element.getTagName().equals("input") && element.getAttribute("type").equals("text")){
+
+		if (element.getTagName().equals("input") && element.getAttribute("type").equals("text")) {
 			// Preenche o valor do Autocomplete
 			element.clear();
 			element.sendKeys(value);
 		}
-		
+
 		/*
-		 *  Tempo do efeito de abertura das opções
-		 *  
-		 *  Obs: Utilizada API de Reflection para retrocompatibilidade
-		 *  	 com versões anteriores do DBehave (1.3.0 por exemplo) 
+		 * Tempo do efeito de abertura das opções
+		 * 
+		 * Obs: Utilizada API de Reflection para retrocompatibilidade com
+		 * versões anteriores do DBehave (1.3.0 por exemplo)
 		 */
-		try{
-			Method waitElementOnlyVisible = getClass().getMethod("waitElementOnlyVisible", new Class[]{Integer.class});
+		try {
+			Method waitElementOnlyVisible = getClass().getMethod("waitElementOnlyVisible", new Class[] { Integer.class });
 			try {
-				waitElementOnlyVisible.invoke(this, new Object[]{1});
+				waitElementOnlyVisible.invoke(this, new Object[] { 1 });
 			} catch (IllegalArgumentException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -136,35 +135,39 @@ public class WebAutoComplete extends WebBase implements AutoComplete {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}catch(NoSuchMethodException e){
+		} catch (NoSuchMethodException e) {
 			waitElement(1);
 		}
-		
-		if(this.selectValue != null){
+
+		if (this.selectValue != null) {
 			value = this.selectValue;
 		}
-		
+
 		selectOnList(elements.get(1), value);
 	}
-	
+
 	/**
-	 * Busca nas tags <li> ou <td> filhas de "element", um texto correspondente a 
-	 * "value" na lista do autocomplete e seleciona-o
+	 * Busca nas tags <li>ou
+	 * <td>filhas de "element", um texto correspondente a "value" na lista do
+	 * autocomplete e seleciona-o
 	 * 
-	 * @param WebElement element Elemento pai da lista de resultados do autocomplete
-	 * @param String value valor a ser procurado na lista
+	 * @param WebElement
+	 *            element Elemento pai da lista de resultados do autocomplete
+	 * @param String
+	 *            value valor a ser procurado na lista
 	 */
-	protected void selectOnList(WebElement element, String value){
-		
+	protected void selectOnList(WebElement element, String value) {
+
 		List<WebElement> elementValue = element.findElements(By.tagName("li"));
-		if(elementValue.size() == 0){
+		if (elementValue.size() == 0) {
 			elementValue = element.findElements(By.tagName("td"));
 		}
-		
+
 		for (WebElement item : elementValue) {
 			if (item.getText().equals(value)) {
 				// Aguarda o segundo elemento ser clicável
 				try {
+					waitElement(1);
 					item.click();
 				} catch (Throwable t) {
 					waitElement(1);
