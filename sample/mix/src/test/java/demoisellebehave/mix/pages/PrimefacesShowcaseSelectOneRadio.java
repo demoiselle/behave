b78f8119c7442db25e414499dfde77868b61fc12
Julian Cesar dos Santos <julian.santos@serpro.gov.br>
2015-07-13 10:33:33 -0300

    Fix imports

--- sample/mix/src/test/java/demoisellebehave/mix/pages/PrimefacesShowcaseSelectOneRadio.java
+++ sample/mix/src/test/java/demoisellebehave/mix/pages/PrimefacesShowcaseSelectOneRadio.java
@@ -1,54 +1,50 @@
 /*
-* Demoiselle Framework
-* Copyright (C) 2013 SERPRO
-* ----------------------------------------------------------------------------
-* This file is part of Demoiselle Framework.
-*
-* Demoiselle Framework is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public License version 3
-* as published by the Free Software Foundation.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License version 3
-* along with this program; if not, see <http://www.gnu.org/licenses/>
-* or write to the Free Software Foundation, Inc., 51 Franklin Street,
-* Fifth Floor, Boston, MA 02110-1301, USA.
-* ----------------------------------------------------------------------------
-* Este arquivo é parte do Framework Demoiselle.
-*
-* O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
-* modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
-* do Software Livre (FSF).
-*
-* Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
-* GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
-* APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
-* para maiores detalhes.
-*
-* Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
-* "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
-* ou escreva para a Fundação do Software Livre (FSF) Inc.,
-* 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
-*/
+ * Demoiselle Framework
+ * Copyright (C) 2013 SERPRO
+ * ----------------------------------------------------------------------------
+ * This file is part of Demoiselle Framework.
+ *
+ * Demoiselle Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Demoiselle.
+ *
+ * O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
 package demoisellebehave.mix.pages;
 
 import br.gov.frameworkdemoiselle.behave.annotation.ElementLocatorType;
 import br.gov.frameworkdemoiselle.behave.annotation.ElementMap;
 import br.gov.frameworkdemoiselle.behave.annotation.ScreenMap;
-import br.gov.frameworkdemoiselle.behave.runner.webdriver.ui.primefaces.PrimeFacesCheckBox;
 import br.gov.frameworkdemoiselle.behave.runner.webdriver.ui.primefaces.PrimeFacesRadioButton;
 
 @ScreenMap(name = "Showcase SelectOneRadio", location = "http://www.primefaces.org/showcase/ui/input/oneRadio.xhtml")
 public class PrimefacesShowcaseSelectOneRadio {
 
-	@ElementMap(name = "Primefaces SelectOneRadio Basic - Console - First Option", locatorType = ElementLocatorType.XPath, locator = {
-			"//div[@id='CONTENTSIDEindent']/div/form/table[1]/tbody/tr/td/table/tbody/tr/td[1]/div[1]",
-			"//div[@id='CONTENTSIDEindent']/div/form/table[1]/tbody/tr/td/table/tbody/tr/td[1]/div[1]/div/input[@type='radio']"
-	})
+	@ElementMap(name = "Primefaces SelectOneRadio Basic - Console - First Option", locatorType = ElementLocatorType.XPath, locator = { "//div[@id='CONTENTSIDEindent']/div/form/table[1]/tbody/tr/td/table/tbody/tr/td[1]/div[1]", "//div[@id='CONTENTSIDEindent']/div/form/table[1]/tbody/tr/td/table/tbody/tr/td[1]/div[1]/div/input[@type='radio']" })
 	private PrimeFacesRadioButton primefacesRadioButton;
 
 }
